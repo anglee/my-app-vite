88fad734e15caa17b9d18c8672ab048d771b14ee
Ang Lee <ang.lee@gmail.com>
2022-12-30 20:10:20 -0500

    Add a few eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest'],
+  plugins: ['import'],
+  rules: {
+    'import/extensions': ['warn', 'never'],
+    'import/order': ['error', { alphabetize: { order: 'asc', caseInsensitive: true } }],
+    'no-nested-ternary': 'error',
+  },
 };
